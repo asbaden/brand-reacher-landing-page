70142fd9eaf428469e310fa0d4a6ea1c4dfea691
gusbaden <gusbaden@gmail.com>
2025-02-12 19:14:54 -0800

    changed some files

--- src/constants/logoUrls.js
+++
@@ -1,7 +0,0 @@
-export const logoUrls = [
-  "https://4qzajju9kzlvxlev.public.blob.vercel-storage.com/logo1-75Fd2x0IavafCg7WZbvn2NCWgtQz1Y.png",
-  "https://4qzajju9kzlvxlev.public.blob.vercel-storage.com/logo2-I9JRlzDiyEBCkLbsmfXfrqGSQNbdHG.png",
-  "https://4qzajju9kzlvxlev.public.blob.vercel-storage.com/logo3-nKFbNaoOAQ8YWoHAe9DQ3S0JX5ku3v.png",
-  "https://4qzajju9kzlvxlev.public.blob.vercel-storage.com/logo4-u9EJcK8G2Nf3Cx4f3WN3lI3NYoBm2J.png",
-  "https://4qzajju9kzlvxlev.public.blob.vercel-storage.com/logo5-PoBs2Wm7HbINKIUOcN1jI3w6yXwsb4.avif"
-]; 
